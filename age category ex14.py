5deb781d48cb4e34e85854305d5b39caa53a2f12
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-10 23:00:04 +0100

    Add files via upload

---
+++ age category ex14.py
@@ -0,0 +1,11 @@
+n=int(input("type the age :"))
+if n>=6 and n<=7 :
+    print("chick")
+elif n>=8 and n<=9 :
+    print("pupil")
+elif n>=10 and n<=11 :
+    print("minimal")
+elif n>=12 :
+    print("cadet")
+else :
+    print("this age category does not exist")
